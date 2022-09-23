f7d8c9a150db95b9b6f26a122dec547ad6d89f35
Homin Lee <ff4500@gmail.com>
2022-09-23 12:47:19 +0900

    fix deploy script to tag too

--- push2gke.sh
+++ push2gke.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#git tag -a $1 -m "add tag for $1"
-#git push --tags
+git tag -a $1 -m "add tag for $1"
+git push --tags
 
 IMAGE_TAG=gcr.io/homin-dev/diagrams_srv
 IMAGE_NAME=$IMAGE_TAG:$1
 IMAGE_NAME_LATEST=$IMAGE_TAG:latest
 
 docker buildx build --platform linux/amd64 -t $IMAGE_NAME .
-# docker push $IMAGE_NAME
+docker push $IMAGE_NAME
 
 docker tag $IMAGE_NAME $IMAGE_NAME_LATEST
 docker push $IMAGE_NAME_LATEST
